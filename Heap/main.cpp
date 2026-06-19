1b90c637a182f38917f5661dbcbfd79be4eafe4a
Mikhil812 <mikhilangre812@gmail.com>
2026-06-19 19:07:02 +0530

    Heap implementation complete as well as 0-based indexing code in notes added

--- Heap/main.cpp
+++ Heap/main.cpp
@@ -41,6 +41,58 @@ void createHeap(int A[], int n, string type){
     
 }
 
+int Delete(int A[], int n){
+    int x = A[1];
+    A[1] = A[n];
+    A[n] = x;           // Storing the deleted element at the last vacant place, its not part of the heap
+
+    int i=1, j=2*i;
+    while(j < n-1){
+        if(A[j+1] > A[j])   // if right child is greater than left child
+            j = j+1;
+        if(A[i] < A[j]){    // if parent is smaller than the max child, then we swap
+            int temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+
+            i = j;
+            j = 2*i;
+        }else
+            break;
+    }   
+    return x;
+}
+
+void heapSort(int A[], int n){
+    for(int i=n; i>1; i--){
+        Delete(A, i);
+    }
+}
+
+void heapify(int A[], int n){
+    // Number of leaf nodes : ceil(n/2) ~ (n+1)/2
+    // First parent : n/2
+    for(int i=n/2; i>=1; i--){
+        int k = i;
+        int j = 2*i;
+        // Deletion logic used for insertion : 
+        while(j <= n){
+            if(j < n && A[j+1] > A[j])
+                j++;                // move to the right child
+            if(A[k] < A[j]){        // compare parent and max child
+                int temp = A[k];
+                A[k] = A[j];
+                A[j] = temp;
+
+                k = j;
+                j = 2*k;
+            }
+            else
+                break;
+        }
+    }
+}
+
 int main() {
     cout<<"Orignal Array (1-based indexing): "<<endl;
     int A[] = {0, 10, 20, 30, 25, 5, 40, 35};      
@@ -51,9 +103,26 @@ int main() {
     createHeap(A, heapSize, "max");
     Print(A, heapSize);
 
-    cout<<"Min Heap : "<<endl;
-    createHeap(A, heapSize, "min");     // This is performing on the max heap array, not on the original array
+    // cout<<"Min Heap : "<<endl;
+    // createHeap(A, heapSize, "min");     // This is performing on the max heap array, not on the original array
+    // Print(A, heapSize);
+
+    cout<<"Deleting an Element : "<<endl;
+    // cout<<Delete(A, heapSize)<<endl;
     Print(A, heapSize);
+
+    cout<<"Heap Sort : "<<endl;
+    heapSort(A, heapSize);          // Calling delete in a loop
+    Print(A, heapSize);
+    
+    cout<<"2nd Orignal Array (1-based indexing): "<<endl;
+    int B[] = {0, 10, 20, 30, 25, 5, 40, 35};      
+    int heapSize2 = sizeof(B)/sizeof(B[0]) - 1;
+    Print(B, heapSize2);
+
+    cout<<"Heapify : "<<endl;
+    heapify(B, heapSize2);
+    Print(B, heapSize2);
 
     return 0;
 }
